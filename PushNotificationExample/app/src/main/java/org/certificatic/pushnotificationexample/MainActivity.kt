bf11dbc239461166aa553a7fbc6920d2bde58f98
Manuel Pérez <manuel.perez@biocatch.com>
2022-07-21 16:07:38 -0500

    Local Notification.

--- PushNotificationExample/app/src/main/java/org/certificatic/pushnotificationexample/MainActivity.kt
+++ PushNotificationExample/app/src/main/java/org/certificatic/pushnotificationexample/MainActivity.kt
@@ -1,7 +1,11 @@
 package org.certificatic.pushnotificationexample
 
 import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -34,6 +38,25 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.channel_name)
+            val id = getString(R.string.default_notification_channel_id)
+            val descriptionText = getString(R.string.channel_description)
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(id, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     private fun requestMessagingPermisions() {
--- PushNotificationExample/app/src/main/java/org/certificatic/pushnotificationexample/firebase/CustomFirebaseMessagingService.kt
+++ PushNotificationExample/app/src/main/java/org/certificatic/pushnotificationexample/firebase/CustomFirebaseMessagingService.kt
@@ -1,13 +1,17 @@
 package org.certificatic.pushnotificationexample.firebase
 
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.certificatic.pushnotificationexample.MainActivity
 import org.certificatic.pushnotificationexample.R
+import java.util.*
 
 class CustomFirebaseMessagingService : FirebaseMessagingService() {
 
@@ -33,15 +37,20 @@ class CustomFirebaseMessagingService : FirebaseMessagingService() {
         //TODO invocar un WS de sincronización
         //TODO Guardar en BD  la información enviada por la notificación
 
-//        val channelId = getString(R.string.default_notification_channel_id)
-//        var notificationBuilder = NotificationCompat.Builder(this, channelId)
-//            .setContentTitle(title)
-//            .setContentText(body)
-//
-//        notificationBuilder.build()
-//
-//        val intent = Intent(this, MainActivity::class.java)
-//        val pendingIntent = PendingIntent.getActivity(this, 999,intent)
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val channelId = getString(R.string.default_notification_channel_id)
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setSmallIcon(R.drawable.notification)
+            .setContentTitle(title)
+            .setContentText(body)
+            .setContentIntent(pendingIntent)
+
+        val notificationId = Random().nextInt()
+
+        val notificationManager = this.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(notificationId, notificationBuilder.build())
 
     }
 
