a20d4b611c2c2e1004a22352574ec7a44397151a
Manuel Pérez <manuel.perez@biocatch.com>
2022-07-23 10:10:35 -0500

    CameraExample + String Base64

--- CameraExample/app/src/main/java/org/certificatic/cameraexample/MainActivity.kt
+++ CameraExample/app/src/main/java/org/certificatic/cameraexample/MainActivity.kt
@@ -1,28 +1,25 @@
 package org.certificatic.cameraexample
 
-import android.app.Activity
-import android.content.Intent
 import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
+
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import java.io.ByteArrayOutputStream
+
 
 class MainActivity : AppCompatActivity() {
-
-    companion object {
-        private const val REQUEST_IMAGE_CAPTURE = 888
-    }
 
     private lateinit var ivImage: ImageView
     private lateinit var btnTakePhoto: Button
 
     private val resultReceiver =
-        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) {
-            setImageToImageView(it)
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            setImageToImageView(bitmap)
         }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,16 +29,44 @@ class MainActivity : AppCompatActivity() {
         this.ivImage = findViewById(R.id.ivImage)
         this.btnTakePhoto = findViewById(R.id.btnTakePhoto)
 
-        this.btnTakePhoto.setOnClickListener { takePhoto() }
+        this.btnTakePhoto.setOnClickListener {
+            this.resultReceiver.launch(null)
+        }
 
     }
 
-    private fun takePhoto() {
-        this.resultReceiver.launch(null)
-    }
 
     private fun setImageToImageView(bitmap: Bitmap?) {
-        this.ivImage.setImageBitmap(bitmap)
+//        if (bitmap != null) {
+//            this.ivImage.setImageBitmap(bitmap)
+//        }
+
+        //(obj != null)
+        //also -> ejecutar métodos de una instancia
+        //appply -> inicializar props instancia despues de constructor
+        //let -> validación de referencias
+
+        bitmap?.let { imagen ->
+            ivImage.setImageBitmap(imagen)
+
+            val byteArrayOutput = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutput)
+
+            val byteArray = byteArrayOutput.toByteArray()
+
+            val stringBase64 = Base64.encodeToString(
+                byteArray,
+                Base64.DEFAULT
+            )
+
+            Log.i("MPS", "Base64:")
+            Log.i("MPS", stringBase64)
+
+            //TODO Subir a WS
+            //TODO Guardar en BD
+
+        }
+
     }
 
 }
